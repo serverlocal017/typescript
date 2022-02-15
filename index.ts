c4d4288eb9663ecfd668ad682e9a09b9e5069659
serverlocal017 <79669115+serverlocal017@users.noreply.github.com>
2022-02-15 23:22:23 +0100

    New TS File

---
+++ index.ts
@@ -0,0 +1,17 @@
+function echo(title: string): string {
+    return title;
+}
+
+function echoYear(year: number): number {
+  return year;
+}
+
+function echoArg(arg: any): any {
+  return arg;
+}
+
+echo('Subscribe');
+echoYear(20);
+
+echoArg('Hello');
+echoArg(2);
