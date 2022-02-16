20e5c3657ed51926f81e4392668057a751d1e833
serverlocal017 <79669115+serverlocal017@users.noreply.github.com>
2022-02-16 10:40:56 +0100

    Class new

--- class.ts
+++ class.ts
@@ -1,9 +1,9 @@
-interface WithId {
-    id: string
+interface WithId<T> {
+    id: T
 }
 
 
-class Track implements WithId {
+class Track implements WithId<string> {
     id: string;
     title: string = 'Untitled';
     artist: string = 'Uknown';
@@ -26,32 +26,34 @@ class Track implements WithId {
 }
 
 
-class Playlist<T extends WithId> {
+class Playlist<T extends WithId<IdType>, IdType> {
     items: T[] = [];
 
     addToPlaylist(newItem: T) {
-        this.items.push(newItem);
+        this.items.push(newItem); 
     }
 
-    removeFromPlaylist(idToRemove: string) {
-        this.items = this.items.filter(item => item.id !== idToRemove),
+    removeFromPlaylist(idToRemove: IdType) {
+        this.items = this.items.filter(item => item.id !== idToRemove);
     }
 }
 
 
-const myTrack = new Track('James Hagen', 'Queen D', 1975);
-const playlist = new Playlist<Track>();
+const myTrack = new Track('1', 'James Hagen', 'Queen D', 1975);
+const playlist = new Playlist<Track, string>();
 
 playlist.addToPlaylist(myTrack);
 
 class VideoClip {
+    id: number;
     name: string = 'Untitled';
 
-    constructor(name: string) {
+    constructor(id: number, name: string) {
+        this.id = id;
         this.name = name;
     }
 }
 
-const videoPlaylist = new Playlist<VideoClip>();
+const videoPlaylist = new Playlist<VideoClip, number>();
 
-videoPlaylist.addToPlaylist(myTrack);
+videoPlaylist.addToPlaylist(new VideoClip(1, 'asdsadsda'))
